390f10399987a593b6da9ea9403485f4336dce28
rishabhsahuu <rishabhsahu106@gmail.com>
2024-10-05 16:05:31 +0530

    drop vndk_package, com.android.vndk.current.on_vendor, remove derp-things

--- device.mk
+++ device.mk
@@ -468,9 +468,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_VNDK_VERSION := current
 PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
-PRODUCT_PACKAGES += \
-    vndk_package \
-    com.android.vndk.current.on_vendor
+#PRODUCT_PACKAGES += \
+#    vndk_package \
+#    com.android.vndk.current.on_vendor
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
